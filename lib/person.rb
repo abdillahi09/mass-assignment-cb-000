c1f7683bee61ed9ccac821c21c228c5224cf8dcd
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-17 01:45:24 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each {|key, value| Person.send(("#{key}="), value)}
+    attributes.each {|key, value| send(("#{key}="), value)}
   end
 
 
